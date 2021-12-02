3fbb39fb078b888d4f85b1f66eb240c784da3513
balzanelli <me@balzanel.li>
2021-12-02 06:40:24 +0000

    feat(day2): solve part 1

---
+++ day2/src/part1.rs
@@ -0,0 +1,71 @@
+type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
+
+struct Submarine {
+    x: i32,
+    y: i32,
+}
+
+impl Default for Submarine {
+    fn default() -> Submarine {
+        Submarine {
+            x: 0,
+            y: 0,
+        }
+    }
+}
+
+impl Submarine {
+    fn forward(&mut self, units: i32) {
+        self.x += units;
+    }
+
+    fn down(&mut self, units: i32) {
+        self.y += units;
+    }
+
+    fn up(&mut self, units: i32) {
+        self.y -= units;
+    }
+
+    fn position(&self) -> i32 {
+        self.x * self.y
+    }
+}
+
+pub fn solve(input: &str) -> Result<i32> {
+    let mut submarine = Submarine::default();
+
+    let directions: Vec<(&str, i32)> = input
+        .lines()
+        .map(|line| {
+            let mut split = line
+                .trim()
+                .split(" ");
+            (split.next().unwrap(), split.next().unwrap().parse().unwrap())
+        })
+        .collect();
+
+    for direction in &directions {
+        match direction.0 {
+            "forward" => submarine.forward(direction.1),
+            "down" => submarine.down(direction.1),
+            "up" => submarine.up(direction.1),
+            _ => {}
+        }
+    }
+    Ok(submarine.position())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn solve() {
+        let input = "forward 5
+            down 5
+            forward 8
+            up 3
+            down 8
+            forward 2";
+        assert_eq!(150, super::solve(input).unwrap())
+    }
+}
