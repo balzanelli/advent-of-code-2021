e43c0184629d7fe1efaaa91e7131e3e24ea04938
balzanelli <me@balzanel.li>
2021-12-02 08:09:58 +0000

    refactor: use standard input stream

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,15 +1,13 @@
+use std::io::Read;
+
 mod part1;
 mod part2;
 
 fn main() {
     println!("advent-of-code-2021: day1");
 
-    let file_path: String = std::env::args()
-        .skip(1)
-        .next()
-        .unwrap_or(String::from("./day1/input"));
-
-    let input = std::fs::read_to_string(file_path)
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input)
         .unwrap();
 
     println!("part1: {}", part1::solve(&input));
