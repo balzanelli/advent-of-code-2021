528613f15c77e6afdec3bd898020acb970cd3dd2
balzanelli <me@balzanel.li>
2021-12-03 00:54:20 +0000

    refactor: run rustfmt

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -7,8 +7,7 @@ fn main() {
     println!("advent-of-code-2021: day1");
 
     let mut input = String::new();
-    std::io::stdin().read_to_string(&mut input)
-        .unwrap();
+    std::io::stdin().read_to_string(&mut input).unwrap();
 
     println!("part1: {}", part1::solve(&input));
     println!("part2: {}", part2::solve(&input));
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,14 +1,15 @@
 use std::io::Read;
 
+mod navigation;
 mod part1;
 mod part2;
+mod route;
 
 fn main() {
     println!("advent-of-code-2021: day2");
 
     let mut input = String::new();
-    std::io::stdin().read_to_string(&mut input)
-        .unwrap();
+    std::io::stdin().read_to_string(&mut input).unwrap();
 
     println!("part1: {}", part1::solve(&input));
     println!("part2: {}", part2::solve(&input));
