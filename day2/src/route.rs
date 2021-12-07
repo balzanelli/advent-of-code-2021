c0434aef83e13ce8cbc07bc61c2da062db1efece
balzanelli <me@balzanel.li>
2021-12-07 07:43:20 +0000

    refactor(day2): use split_whitespace

--- day2/src/route.rs
+++ day2/src/route.rs
@@ -17,7 +17,7 @@ impl FromStr for Point {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut split = s.trim().split(" ");
+        let mut split = s.trim().split_whitespace();
         let direction = Direction::from_str(split.next().unwrap()).unwrap();
         let units = split.next().unwrap().parse().unwrap();
         Ok(Self::new(direction, units))
